32f791c06a6c1809fb474a4d8b288ec28a4df858
sivanathan vithurshan <vithurshans.22@uom.lk>
2025-03-29 10:30:49 +0530

    fir

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,52 @@
 'use strict';
 
+const { json, text } = require("body-parser");
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
+    .get(async function (req, res){
+      const {stock,like} = req.query;
+      console.log(typeof(stock));
+      console.log(req.query);
+      if(typeof(stock)===typeof("dd")){
+        //console.log("vanthutten");
+        if(like){
+          const ress = await fetch(`https://stock-price-checker.freecodecamp.rocks/api/stock-prices?stock=${stock}&like=${like}`);
+          const body= await ress.json();
+          console.log(body);
+          ress != null ? res.status(200).json(body):res.status(400).type(text).send("invalid");
+        }else{
+          const ress = await fetch(`https://stock-price-checker.freecodecamp.rocks/api/stock-prices?stock=${stock}`);
+          const body= await ress.json();
+          console.log(body);
+          ress != null ? res.status(200).json(body):res.status(400).type(text).send("invalid");
+        }
+        
+        
+
+      }else if (typeof(stock)==="object"){
+        if(like){
+          const ress = await fetch(`https://stock-price-checker.freecodecamp.rocks/api/stock-prices?stock=${stock[0]}&stock=${stock[1]}&like=true`);
+          const body= await ress.json();
+          console.log(body);
+          ress != null ? res.status(200).json(body):res.status(400).type(text).send("invalid");
+        }else{
+          const ress = await fetch(`https://stock-price-checker.freecodecamp.rocks/api/stock-prices?stock=${stock[0]}&stock=${stock[1]}`);
+          const body= await ress.json();
+          console.log(body);
+          ress != null ? res.status(200).json(body):res.status(400).type(text).send("invalid");
+        }
+        
+        //const body= await ress.json();
+        // console.log(body);
+        //ress != null ? res.status(200).json(body):res.status(400).type(text).send("invalid");
+      }else{
+        res.status(500).type(text).send("internal server error");
+      }
+      
+
+      
       
     });
     
